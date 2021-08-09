35ddb84b20a316cbd5cbc0e35353a79d9daa0d7a
kenkwok0216 <74519929+kenkwok0216@users.noreply.github.com>
2021-08-09 21:17:38 +0800

    Event Modified

--- src/me/kenandwicky/tetris/Listener/EventListener.java
+++ src/me/kenandwicky/tetris/Listener/EventListener.java
@@ -1,13 +1,17 @@
 package me.kenandwicky.tetris.Listener;
 
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
+import me.kenandwicky.tetris.GameLoop.Game;
+import me.kenandwicky.tetris.Tetris;
 import me.kenandwicky.tetris.Board.Board;
 import me.kenandwicky.tetris.Board.SettingsManager;
 
@@ -29,5 +33,68 @@ public class EventListener implements Listener {
     	}
     	Board.initialize(e.getPlayer(), settings);
     }
+    
+    @EventHandler
+    public void onPlayerClick(PlayerInteractEvent e) {
+    	if(Tetris.isStart == false) {
+    		return;
+    	}
+    	
+    	if(e.getAction().equals(Action.LEFT_CLICK_AIR)) {
+    		Game.rotateLeft();
+    		return;
+    	} else if(e.getAction().equals(Action.RIGHT_CLICK_AIR)) {
+    		Game.rotateRight();
+    		return;
+    	}
+    	
+    }
+    
+    
+    
+    
+    @EventHandler
+    public static void onPlayerMovement(PlayerMoveEvent e) {
+    	if(Tetris.isStart == false) {
+    		return;
+    	}
+    	
+    	Location fromLocation = e.getFrom();
+    	Location toLocation = e.getTo();
+    	
+    	double xDiff = Math.abs(toLocation.getX() - fromLocation.getX());
+    	double yDiff = Math.abs(toLocation.getY() - fromLocation.getY());
+    	double zDiff = Math.abs(toLocation.getZ() - fromLocation.getZ());
+    	
+    	if (xDiff > 0 || yDiff > 0 || zDiff > 0) {
+    		e.getPlayer().teleport(fromLocation.setDirection(toLocation.getDirection()));
+    	}
+    	
+    	if (yDiff > 0) {
+    		Game.holdPiece();
+    	}
+    	
+    	if (zDiff > xDiff) {
+    		if (toLocation.getZ() - fromLocation.getZ() < 0) {
+    			Game.moveDown();
+    		} else {
+    			return;
+    		}
+    		return;
+    	}
+    	
+    	if (xDiff > zDiff) {
+    		if (toLocation.getX() - fromLocation.getX() > 0) {
+    			Game.moveLeft();
+    			e.getPlayer().sendMessage("Left");    			
+    		} else {
+    			Game.moveRight();
+    			e.getPlayer().sendMessage("Right");
+    		}
+    		
+    		return;	
+    	}   	    	
+    }
+    
 	
 }
